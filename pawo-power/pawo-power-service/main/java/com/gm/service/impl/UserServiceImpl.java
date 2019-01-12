90e306a121385470ed9d7bf56e0051fce5e0c64c
datoubangzhu <2419475724@qq.com>
2019-01-12 17:14:05 +0800

    readme修改，获取用户列表修改

--- pawo-power/pawo-power-service/main/java/com/gm/service/impl/UserServiceImpl.java
+++ pawo-power/pawo-power-service/main/java/com/gm/service/impl/UserServiceImpl.java
@@ -6,9 +6,13 @@
 
 package com.gm.service.impl;
 
+import com.google.common.collect.Lists;
+
 import com.gm.po.UserPo;
 import com.gm.service.IUserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -28,14 +32,30 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class UserServiceImpl implements IUserService{
+
+
+    private final static String USER_KEY = "pawo:auth:user";
+
+    private HashOperations<String,Object,Object> hashOperations;
+
+    @Autowired
+    public UserServiceImpl(HashOperations<String, Object, Object> hashOperations) {
+        this.hashOperations = hashOperations;
+    }
+
+
+
+
     @Override
     public List<UserPo> listUserPo() {
+        List<UserPo> userPos = Lists.newArrayList();
+        List<Object> userPoList = hashOperations.values(USER_KEY);
+        for(Object o:userPoList){
+
+        }
+
+
         log.info("执行UserServiceImpl 中 listUserPo方法---------");
         return null;
     }
-
-    public void test(){
-
-    }
-
 }
